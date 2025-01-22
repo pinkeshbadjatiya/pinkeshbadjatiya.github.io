39d591a6fed6c50b1e368cc34554b59f296b4cce
Pinkesh Badjatiya <pinkesh@cloudysoft.com>
2025-01-22 19:36:11 +0530

    update

--- static/infer.js
+++ static/infer.js
@@ -22,15 +22,15 @@ function scrollToBottomOfChat() {
     lastDiv.scrollIntoView();
 }
 
-function fetch(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.onload = () => resolve(xhr.responseText);
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send();
-    });
-}
+// function fetch(url) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open("GET", url);
+//         xhr.onload = () => resolve(xhr.responseText);
+//         xhr.onerror = () => reject(xhr.statusText);
+//         xhr.send();
+//     });
+// }
 
 function askDonnaTheAssistant(userQuestion, chatObj) {
     /*
